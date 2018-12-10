1537afe960b26900afb33dc810de6d5d2f3f1cb9
DarlanJurak <darlan.jurak@gmail.com>
2018-12-10 13:45:28 -0200

    added ROS to JSHOP stream (problem file overwriting)

--- USV_GuidanceSystem.py
+++ USV_GuidanceSystem.py
@@ -5,15 +5,52 @@ import  rospy
 from    std_msgs.msg import String
 # import  sensor_msgs
 from    sensor_msgs.msg import JointState
+from    gazebo_msgs.msg import ModelStates
 
 flag_direction = 0
+
+# information about collision
+string = '        (at         intruder    GC_B3       )        \n'
+
+def ros_feedback_callback(data):
+    # rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.pose[3].position.x)
+    # rospy.loginfo("Airboat  x: %d y: %d", data.pose[1].position.x, data.pose[1].position.y)
+    # rospy.loginfo("Buyo     x: %d y: %d", data.pose[3].position.x, data.pose[3].position.y)
+
+    models_position_airboat     = 1
+    models_position_intruder    = 2
+    
+    if ((abs(data.pose[models_position_intruder].position.x - data.pose[models_position_airboat].position.x) < 10) and 
+        (abs(data.pose[models_position_intruder].position.y - data.pose[models_position_airboat].position.y) < 10)):
+
+        print "Near"
+        
+        # Copy file content as a string vector
+        f           = open("/home/darlan/Darlan/Projects/Artificial-Inteligence/Planning/Hierarchical-Task-Network/jshop2/examples/colregs/problem.pddl", "r")
+        content     = f.readlines()
+        f.close()
+
+        # print content
+
+        # # Prepare for overwriting last 6 lines of the file with new information about possible collision
+        # print content[-6]
+        content[-6] = string
+        # print content[-6]
+
+        # Write changes on file (overwriting)
+        f = open("/home/darlan/Darlan/Projects/Artificial-Inteligence/Planning/Hierarchical-Task-Network/jshop2/examples/colregs/problem.pddl", "w")
+        for line in content:
+            f.write(line)
+        f.close()
 
 def airboat_guider():
 
     global flag_direction
+    global string
 
     # ROS node initialization
     rospy.init_node('airboat_guider', anonymous=True)
+    rospy.Subscriber("/gazebo/model_states", ModelStates, ros_feedback_callback)
 
     # Publishers for joint turning and thruster control
     pub_joint               = rospy.Publisher('/airboat/joint_states',      JointState, queue_size=10)
@@ -35,28 +72,30 @@ def airboat_guider():
 
     while not rospy.is_shutdown():
 
-        # read JSHOP plan each 10 seconds
+        # read JSHOP plan each 5 seconds
         counter = counter +1
-        if counter == 1000*10:
+        if counter == 1000*5:
             counter = 0
-            JSHOP_to_ROS()
+            # JSHOP_to_ROS()
 
+        # boat direction control
         if flag_direction == 0:
             thruster_position_vector    = [4]   # go ahead
-            print "keep current motion"
+            # print "go ahead"
+
         elif flag_direction == 1:
-            joint_position_vector       = [1]   # turn right
-            print "go right"
+            joint_position_vector       = [-1]   # turn right
+            # print "go right"
+
         elif flag_direction == 2:
-            joint_position_vector       = [-1]  # turn left
-            print "go left"
+            joint_position_vector       = [1]  # turn left
+            # print "go left"
 
         joint_state_msg.position        = ['fwd_joint']
         joint_state_msg.position        = joint_position_vector
 
         thruster_command_msg.name       = ['fwd']
         thruster_command_msg.position   = thruster_position_vector
-        
 
         pub_joint.publish(joint_state_msg)
         pub_thruster.publish(thruster_command_msg)
@@ -82,16 +121,16 @@ def JSHOP_to_ROS():
 
         if new_direction_desired == 'c':
             flag_direction = 1                              # go right flag
-            print "Go right"
+            # print "Go right"
         elif new_direction_desired == 'a':
             flag_direction = 2                              # go left flag
-            print "Go left"
+            # print "Go left"
+        else: 
+            flag_direction = 0
+            # print "Go ahead"
 
 if __name__ == '__main__':
     try:
         airboat_guider()
     except rospy.ROSInterruptException:
         pass
-
-
-
